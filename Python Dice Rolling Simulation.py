dc3fcf621d71bcff4d1c3c729be888c24ac8819c
volatileSaint <30358830+volatileSaint@users.noreply.github.com>
2017-08-06 03:58:17 -0600

    Update Python Dice Rolling Simulation.py

--- Python Dice Rolling Simulation.py
+++ Python Dice Rolling Simulation.py
@@ -2,15 +2,20 @@ import random
 import time
 
 def dieroll():
+    #This is the amount of rolls you have already done. Each time you roll, this will change.
     times_rolled = 0
-    die_amount = input("\nHow many dice would you like to throw? ")
+    dice_amount = input("\nHow many dice would you like to throw? ")
     roll_amount = input("\nHow many rolls would you like to throw? ")
+    #This while loop lets keep rolling your dice until you have rolled the amount of rolls you have chosen.
     while times_rolled < int(roll_amount):
         print ("\nRolling the dice...")
+        #This sleep function is for dramatic effect:
         time.sleep(2.5)
-        #This sleep function is for dramatic effect.
-        roll = random.randrange(int(die_amount), int(die_amount)*6)
+        #This code randomizes the what number you will get from the all of your dice.
+        #When you input the number of dice of dice_amount, this code will provide you the new range for the amount of dice you choose.
+        roll = random.randrange(int(dice_amount), int(dice_amount)*6)
         print ("\nYou rolled a {}!".format(roll))
+        #This adds to the amount of rolls you have already done:
         times_rolled += 1
     rollagain()
 
@@ -25,8 +30,9 @@ def rollagain():
         exit()
     else:
         print ("\nI cannot understand that, unfortunately.")
+        #This sleep function is meant to give them time to read:
         time.sleep(2)
-        #This sleep function is meant to give them time to read.
+        #Repeats the function because the system cannot understand the previous user command.
         rollagain()
 
     
